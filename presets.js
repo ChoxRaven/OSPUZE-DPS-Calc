7d1ad622db40ca60b13f93c48f5a969c6723fb12
Chox Raven <mohdsamir048@gmail.com>
2025-07-03 23:33:18 +0400

    reworked the preset system to use a seperate file

---
+++ presets.js
@@ -0,0 +1,115 @@
+window.presets = {
+    // Light weapons
+    V9S: {
+    displayName: 'V9S',
+    damage: 40, critMult: 1.5, magazine: 20, fireRate: 360, reloadTime: 1.5,
+    burstRate: 0, burstCount: 0, firemode: 'Standard',
+    falloff: true, falloffMult: 0.62, falloffStart: 10, falloffEnd: 15
+    },
+    Berreta93R: {
+    displayName: '93R',
+    damage: 26, critMult: 1.5, magazine: 21, fireRate: 950, reloadTime: 1.75,
+    burstRate: 220, burstCount: 3, firemode: 'Standard',
+    falloff: true, falloffMult: 0.4, falloffStart: 32, falloffEnd: 40
+    },
+    M11: {
+    displayName: 'M11',
+    damage: 16, critMult: 1.5, magazine: 40, fireRate: 1000, reloadTime: 2.2,
+    burstRate: 0, burstCount: 0, firemode: 'Standard',
+    falloff: true, falloffMult: 0.52, falloffStart: 10, falloffEnd: 17.5
+    },
+    XP54: {
+    displayName: 'XP-54',
+    damage: 17, critMult: 1.5, magazine: 34, fireRate: 850, reloadTime: 2.5,
+    burstRate: 0, burstCount: 0, firemode: 'Standard',
+    falloff: true, falloffMult: 0.52, falloffStart: 22.5, falloffEnd: 32.5
+    },
+    ARN220: { //TODO, PLACEHOLDER
+    displayName: 'ARN-220',
+    damage: 17, critMult: 1.5, magazine: 30, magazines: 2, fireRate: 720, reloadTime: 2.8, reloadSwapTime: 0.75,
+    burstRate: 0, burstCount: 0, firemode: 'Standard',
+    falloff: true, falloffMult: 0.72, falloffStart: 37.5, falloffEnd: 42.5
+    },
+    LH1: {
+    displayName: 'LH1',
+    damage: 40, critMult: 2, magazine: 15, fireRate: 270, reloadTime: 2.85,
+    burstRate: 0, burstCount: 0, firemode: 'Standard',
+    falloff: true, falloffMult: 0.75, falloffStart: 50, falloffEnd: 55
+    },
+    SR84: {
+    displayName: 'SR-84',
+    damage: 118, critMult: 2, magazine: 6, fireRate: 46, reloadTime: 3.35,
+    burstRate: 0, burstCount: 0, firemode: 'Standard',
+    falloff: true, falloffMult: 0.75, falloffStart: 80, falloffEnd: 100
+    },
+    RECURVEBOW: { //TODO, PLACEHOLDER
+    displayName: 'Recurve Bow',
+    damage: 60, fullDamage: 124, critMult: 1.5, magazine: 1, fireRate: 78, reloadTime: 0.75,
+    burstRate: 0, burstCount: 0, firemode: 'Charged',
+    falloff: false, falloffMult: 0.75, falloffStart: 80, falloffEnd: 100
+    },
+
+    // Medium weapons
+    R357: {
+    displayName: 'R.357', // The goat
+    damage: 74, critMult: 2, magazine: 6, fireRate: 140, reloadTime: 2,
+    burstRate: 0, burstCount: 0, firemode: 'Standard',
+    falloff: true, falloffMult: 0.4, falloffStart: 27.5, falloffEnd: 44
+    },
+    AKM: {
+    displayName: 'AKM',
+    damage: 20, critMult: 1.5, magazine: 32, fireRate: 600, reloadTime: 2.35,
+    burstRate: 0, burstCount: 0, firemode: 'Standard',
+    falloff: true, falloffMult: 0.55, falloffStart: 30, falloffEnd: 37.5
+    },
+    FCAR: {
+    displayName: 'FCAR',
+    damage: 22, critMult: 1.5, magazine: 25, fireRate: 540, reloadTime: 2.1,
+    burstRate: 0, burstCount: 0, firemode: 'Standard',
+    falloff: true, falloffMult: 0.55, falloffStart: 35, falloffEnd: 40
+    },
+    FAMAS: {
+    displayName: 'FAMAS',
+    damage: 23, critMult: 1.5, magazine: 27, fireRate: 1050, reloadTime: 2.5,
+    burstRate: 220, burstCount: 3, firemode: 'Burst',
+    falloff: true, falloffMult: 0.5, falloffStart: 35, falloffEnd: 42.5
+    },
+    Pike556: {
+    displayName: 'Pike-556',
+    damage: 50, critMult: 1.5, magazine: 12, fireRate: 200, reloadTime: 3.25,
+    burstRate: 0, burstCount: 0, firemode: 'Standard',
+    falloff: true, falloffMult: 0.75, falloffStart: 45, falloffEnd: 50
+    },
+    CB01Repeater: {
+    displayName: 'CB-01 Repeater',
+    damage: 84, critMult: 1.5, magazine: 8, fireRate: 73, reloadTime: 5.05,
+    burstRate: 0, burstCount: 0, firemode: 'Standard',
+    falloff: true, falloffMult: 0.7, falloffStart: 40, falloffEnd: 45
+    },
+
+    // Heavy weapons
+    Akimbo50: {
+    displayName: '.50 Akimbo',
+    damage: 44, critMult: 2, magazine: 14, fireRate: 230, reloadTime: 1.8,
+    burstRate: 0, burstCount: 0, firemode: 'Standard',
+    falloff: true, falloffMult: 0.5, falloffStart: 35, falloffEnd: 42
+    },
+    M60: {
+    displayName: 'M60',
+    damage: 20, critMult: 1.5, magazine: 70, fireRate: 580, reloadTime: 3.55,
+    burstRate: 0, burstCount: 0, firemode: 'Standard',
+    falloff: true, falloffMult: 0.5, falloffStart: 25, falloffEnd: 35
+    },
+    LewisGun: {
+    displayName: 'Lewis Gun',
+    damage: 22, critMult: 1.5, magazine: 47, fireRate: 500, reloadTime: 3.55,
+    burstRate: 0, burstCount: 0, firemode: 'Standard',
+    falloff: true, falloffMult: 0.67, falloffStart: 35, falloffEnd: 40
+    },
+    KS23: {
+    displayName: 'KS-23',
+    damage: 100, critMult: 1, magazine: 6, fireRate: 75, reloadTime: 4.36,
+    burstRate: 0, burstCount: 0, firemode: 'Standard',
+    falloff: true, falloffMult: 0.7, falloffStart: 18, falloffEnd: 23
+    }
+};
